047de417b2c4b4525eff1cd50e1e83f9169a7683
XumarN <khumarnabiyeva14@mail.ru>
2022-12-16 12:42:49 +0400

    javas

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+
+function select(){
+	var img_actor = document.getElementById('actorselect').value
+	console.log(car_select)
+	document.getElementById('img_actor').src="img/"+actorselect+".jpg"
+}
